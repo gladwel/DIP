c230bcea0db8a4bb41c8b53cbbcc5d29e9a4966e
gladwel <petushok9965@gmail.com>
2023-10-08 14:21:20 +0200

    Files restructure

--- TODO.py
+++
@@ -1,3 +0,0 @@
-# TODO in 329 fix sentence amount problems (example id=396)
-# TODO in 380 fix to make the same as code
-# TODO in 147 make description of the plots (The distribution appears to be right-skewed, as the mean...)
